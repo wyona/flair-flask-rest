b18e895d1d62af27c816b3968d586f11a353f17b
Michael Wechner <michael.wechner@wyona.com>
2022-04-02 11:23:28 +0200

    train model started

---
+++ train-model.py
@@ -0,0 +1,22 @@
+import flair.datasets
+
+corpus = flair.datasets.UD_ENGLISH()
+
+print('')
+
+# print the number of Sentences in the train split
+print('Length corpus train: ',len(corpus.train))
+
+# print the number of Sentences in the test split
+print('Length corpus test: ',len(corpus.test))
+
+# print the number of Sentences in the dev split
+print('Length corpus dev: ',len(corpus.dev))
+
+# print the first Sentence in the training split
+print('')
+print(corpus.test[0])
+
+# print the first Sentence in the training split
+print('')
+print('PoS tags: ',corpus.test[0].to_tagged_string('pos'))
