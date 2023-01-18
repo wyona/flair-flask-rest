98312b923376725e94884af2eddcb1184b2c6fe5
Michael Wechner <michael.wechner@wyona.com>
2023-01-18 12:00:07 +0100

    use waitress instead app.run

--- flair-ner-rest.py
+++ flair-ner-rest.py
@@ -44,4 +44,7 @@ def namedEntityRecognition():
     return jsonify(response), 200
  
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    logger.info(f"Starting server ...")
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
+    #app.run(host='0.0.0.0', port=5000)
