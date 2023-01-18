44a28f15792c03957379ac137fe9ebb7a2c26897
Michael Wechner <michael.wechner@wyona.com>
2023-01-18 11:57:41 +0100

    use logger instead print

--- flair-ner-rest.py
+++ flair-ner-rest.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import abort, Flask, jsonify, request
 from flair.models import SequenceTagger
 from flair.data import Sentence
@@ -12,6 +14,9 @@ class NamedEntity():
         return {"text": self.t, "tag": self.tag, "score": self.s}
  
 app = Flask(__name__)
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
  
 tagger = SequenceTagger.load('ner')
  
@@ -19,18 +24,19 @@ tagger = SequenceTagger.load('ner')
 def namedEntityRecognition():
     if not request.json or not 'message' in request.json:
         abort(400)
+
     message = request.json['message']
     sentence = Sentence(message)
     tagger.predict(sentence)
-    print('Analyzed: ', sentence)
+    logger.info(f"Analyzed: {sentence}")
 
     entities=[]
     for spanOfEntity in sentence.get_spans('ner'):
-        print('Entity: ', spanOfEntity)
-        print('Labels: ', spanOfEntity.labels)
-        print('Tag: ', spanOfEntity.tag)
+        logger.info(f"Entity: {spanOfEntity}")
+        logger.info(f"Labels: {spanOfEntity.labels}")
+        logger.info(f"Tag: {spanOfEntity.tag}")
         namedEntity = NamedEntity(spanOfEntity.text, spanOfEntity.tag, spanOfEntity.score)
-        print('Named entity: ', namedEntity.to_dict())
+        logger.info(f"Named entity: {namedEntity.to_dict()}")
         entities.append(namedEntity.to_dict())
         #entities.append(spanOfEntity.to_dict())
 
